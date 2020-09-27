65c5947f3275884b82641cdf2397c61c654b9b38
Bhard27 <pranjalb2709@gmail.com>
2020-09-27 14:24:15 +0530

    scripts modified

--- ML/scraping/gainers/daily_top_gainers.py
+++ ML/scraping/gainers/daily_top_gainers.py
@@ -15,15 +15,30 @@ import matplotlib.pyplot as plt
 def daily_gainers():
     dfs = pd.read_html('https://money.rediff.com/gainers/bse/daily',header=0)
     for df in dfs[:-1]:
-        print(df)
+        df=df
 
     df['% Change'] = df['% Change'].str.replace(' ', "")
-    df1 = df[['Company', '% Change']]
-    print(df1)
+    df1 = df[['Company', '% Change', 'Current Price (Rs)']][:10]
+    data = {}
+    col = list(df1)
+    for i in range(10):
+        current = 'Company {}'.format(i+1)
+        data[current] = {}
+        c=0
+        for j in col:
+            if c==0:
+                data[current]['Company Name'] = df[j][i]
+            elif c==1:
+                data[current]['% Change'] = df[j][i]
+            else:
+                data[current]['Current Price (Rs)'] = df[j][i]
+                c+=1
+    return data
+    
     df1.to_csv('daily_top_gainers.csv',index=False)
     
     
-daily_gainers()
+#daily_gainers()
 
 
 
--- ML/scraping/losers/weekly_top_losers.py
+++ ML/scraping/losers/weekly_top_losers.py
@@ -14,13 +14,29 @@ import matplotlib.pyplot as plt
 def weekly_losers():
     dfs = pd.read_html('https://money.rediff.com/losers/bse/weekly',header=0)
     for df in dfs[:-1]:
-        print(df)
+        df = df
 
-    df1 = df[['Company', '% Change']]
-    print(df1)
+#    df['% Change'] = df['% Change'].str.replace(' ', "")
+    df1 = df[['Company', '% Change', 'Current Price (Rs)']][:10]
+    data = {}
+    col = list(df1)
+    for i in range(10):
+        current = 'Company {}'.format(i+1)
+        data[current] = {}
+        c=0
+        for j in col:
+            if c==0:
+                data[current]['Company Name'] = df[j][i]
+            elif c==1:
+                data[current]['% Change'] = df[j][i]
+            else:
+                data[current]['Current Price (Rs)'] = df[j][i]
+                c+=1
+    return data
+    
     df1.to_csv('weekly_top_losers.csv', index=False)
 
-weekly_losers()
+#weekly_losers()
 
 def plot_weekly_losers():
     plt.style.use('fivethirtyeight')
