a74a8736b31eeb85957dfb884104897187ad1cee
Bhard27 <pranjalb2709@gmail.com>
2020-10-23 22:31:55 +0530

    new commit

--- ML/scraping/historical_data.py/historical_data.py
+++ ML/scraping/historical_data.py/historical_data.py
@@ -12,8 +12,6 @@ from datetime import datetime, timedelta
 
 class financeData:
     """
-      getYahooData fetches real-time Yahoo! finance data(historical data)
-      through Yahoo! API and returns a single set of entry used for testing
       -------------
       input:
         keyword
@@ -22,20 +20,20 @@ class financeData:
       output:
          Difference in the stock price over the time
       -------------
-      
+
     """
     def getYahooData(self, keyword, start_date):
         end_date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
         #print(start_date)
         #print(end_date)
         df = dr.data.get_data_yahoo(keyword, start= start_date, end=end_date)
-        
+
         #return df.head()
         #df = df.rename(columns = {'Adj Close':'Close'})
         #print(df)
-        for i in list(df): 
+        for i in list(df):
             close_prices = df['Close'].values.tolist()
-        
+
         a = close_prices[-1] #price of stock today
         b = close_prices[0] # Price of stock on the day user wants to know
         #print(a)
@@ -45,5 +43,5 @@ class financeData:
         #print(df)
         #historical_data = yahoo.get_historical(start_date, end_date)
         #return historical_data
-            
+
    #getYahooData('', 'tsla', '2020-03-18')
