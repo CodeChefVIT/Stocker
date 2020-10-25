0d755fc1045d5d37577489f87d26e70ede535813
Bhard27 <pranjalb2709@gmail.com>
2020-10-26 01:51:56 +0530

    industry-wise best stocks script finished

--- ML/scraping/Sector/sectors_scraping.py
+++ ML/scraping/Sector/sectors_scraping.py
@@ -10,34 +10,59 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import matplotlib.pyplot as plt
 
-param = ['automotive', 'banking-finance', 'cement-construction', 'chemicals' , 'conglomerates',
-'consumer-durable',
-'consumer-non-durable',
-'engineering',
-'food-beverage',
-'gold-etf',
-'technology',
-'manufacturing',
-'media',
-'metals-mining',
-'miscellaneous',
-'oil-gas',
-'pharmaceuticals',
-'retail-real-estate',
-'services',
-'telecom',
-'tobacco',
-'utilities']
+
+param = ['auto',
+         'bankex',
+         'bse-cg',
+         'bse-cd',  
+         'bsefmc',
+         'bse-hc',
+         'bse-it',
+         'metal',
+         'oilgas',
+         'bsepsu',
+         'realty',
+         'teck',
+         'power',
+         'cpse',
+         'infra',
+         'basmtr',
+         'cdgs',
+         'energy',
+         'fin',
+         'indstr',
+         'telcom',
+         'utilities']
+##This is the list of sectors
 
 
+def sectors(i):
+    dfs = pd.read_html('https://money.rediff.com/sectors/bse/' + i,header=0)
+    for df in dfs[:-1]:
+        #print(df)
+        df1 = df[['Company', '% Change', 'Current Price (Rs)']]
+        sorted_df1 = df1.sort_values(by='% Change', ascending=False)
+        sorted_df1.reset_index(inplace = True, drop = True)
+    #final_df = sorted_df1[:5]
+       # print(final_df)
+        
+ 
+    
+    data = {}
+    col = list(sorted_df1)
+    for i in range(10):
+        current = 'Company {}'.format(i+1)
+        data[current] = {}
+        c=0
+        for j in col:
+            if c==0:
+                data[current]['Company Name'] = sorted_df1[j][i]
+            elif c==1:
+                data[current]['% Change'] = sorted_df1[j][i]
+            else:
+                data[current]['Current Price (Rs)'] = sorted_df1[j][i]
+                c+=1
+    return data
+    
+##Gives the best stocks to invest in the sector
 
-def sectors():
-    DF_list = list()
-
-    for i in param:
-        dfs = pd.read_html('http://www.moneycontrol.com/india/stockmarket/sector-classification/marketstatistics/nse/'+ i +'.html?classic=true',header=0)
-        df1 = dfs[['Company Name', '%Chg']]
-        print(dfs)
-
-###Still working on it
-sectors()
